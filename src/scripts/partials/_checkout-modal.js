2c23309a8056d4eae83748bb5f420ba98e6b86e4
Ilya khvesevich <rairetdev@gmail.com>
2018-10-31 12:35:42 +0200

    concert page responsive

--- src/scripts/partials/_checkout-modal.js
+++ src/scripts/partials/_checkout-modal.js
@@ -15,9 +15,3 @@ $(window).click(function (event) {
     }
 });
 
-// // When the user clicks anywhere outside of the modal, close it
-// window.onclick = function(event) {
-//     if (event.target === modal) {
-//         modal.style.display = "none";
-//     }
-// };
