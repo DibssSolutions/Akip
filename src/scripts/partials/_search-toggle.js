ace926a417d4095af6ef97554f1146f60340e701
KopylovVitalik <vitalik.kopylov@gmail.com>
2018-11-01 12:18:01 +0200

    search desctop, merge

--- src/scripts/partials/_search-toggle.js
+++ src/scripts/partials/_search-toggle.js
@@ -1,4 +1,10 @@
 $(".js-search-trigger").on("click", function(e) {
 	$(".js-search").addClass("is-open");
-	console.log(e.target);
+	$(".js-search-input").focus();
+	$('body').addClass("is-search-open");
+});
+
+$(".js-search-close").on("click", function(e) {
+	$(".js-search").removeClass("is-open");
+	$('body').removeClass("is-search-open");
 });
